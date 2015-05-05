65cda4b54cac06841443775b7c40d92821d783dd
Alex <alex-postkasten@web.de>
2015-05-05 12:45:54 +0200

    Frage implementiert

--- studienberatung.java
+++ studienberatung.java
@@ -3,5 +3,9 @@ import java.util.*;
 public class Studienberatung {
     public static void main(String[] args) {
         Scanner scanner = new Scanner();
+        String answer;
+        System.out.println("Hast Du Abitur?");
+        if(Scanner.hasNext()) answer = Scanner.next();
+        
     }
 }
